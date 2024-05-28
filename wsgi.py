b2c4109c77194e4ce4753dd206bab707683e5405
blakebabikianbentley <blakebabikian@gmail.com>
2024-05-28 11:22:00 -0400

    Import app

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, url_for, jsonify
 import numpy as np
 import json
+from app import app, port
 
 
 @app.route('/', methods=['GET', 'POST'])
